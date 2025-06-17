60fca23df2402cad0724639bea8e79356f69848d
jinyu <jinyu.xie@my.jcu.edu.au>
2025-06-17 16:18:02 +0800

    create the new dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,11 @@
+COLOR_CODES_NAME={"Cornsilk1":"#fff8dc",
+                  "Cornsilk2":"#eee8cd",
+                  "Cornsilk3":"#cdc8b1",
+                  "Cornsilk4":"#8b8878",
+                  "Cosmic Cobalt":"#2e2d88",
+                  "Cosmic Latte":"#fff8e7",
+                  "Cotton Candy":"#ffbcd9",
+                  "Cream":"#fffdd0",
+                  "Crimson":"#dc143c",
+                  "Crystal":"#a7d8de"
+}
