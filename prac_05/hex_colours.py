24902a815c24ec948dcf4c6874d41e5b3f18af0a
jinyu <jinyu.xie@my.jcu.edu.au>
2025-06-17 16:42:01 +0800

    create the main function ,according to the user inpout show the corresponding code

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,11 +1,21 @@
-COLOR_CODES_NAME={"Cornsilk1":"#fff8dc",
-                  "Cornsilk2":"#eee8cd",
-                  "Cornsilk3":"#cdc8b1",
-                  "Cornsilk4":"#8b8878",
-                  "Cosmic Cobalt":"#2e2d88",
-                  "Cosmic Latte":"#fff8e7",
-                  "Cotton Candy":"#ffbcd9",
-                  "Cream":"#fffdd0",
-                  "Crimson":"#dc143c",
-                  "Crystal":"#a7d8de"
+COLOR_CODES_NAME={"cornsilk1":"#fff8dc",
+                  "cornsilk2":"#eee8cd",
+                  "cornsilk3":"#cdc8b1",
+                  "cornsilk4":"#8b8878",
+                  "cosmic Cobalt":"#2e2d88",
+                  "cosmic Latte":"#fff8e7",
+                  "cotton Candy":"#ffbcd9",
+                  "cream":"#fffdd0",
+                  "crimson":"#dc143c",
+                  "crystal":"#a7d8de"
 }
+
+def main():
+    color_name=input("Enter the color name:").strip().lower()
+    while color_name!="":
+         if color_name in COLOR_CODES_NAME:
+             print(f"The color is {color_name} and the code is {COLOR_CODES_NAME[color_name]}")
+         else:
+             print(f"sorry, your color code {color_name} is invalid ")
+         color_name=input("Enter the color name:").strip().lower()
+main()
