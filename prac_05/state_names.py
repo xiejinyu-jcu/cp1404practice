9e22602c2157609b587494e9c4e5966028bbe7be
jinyu <jinyu.xie@my.jcu.edu.au>
2025-06-18 15:09:28 +0800

    change the LBYL method, use the try-except

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,6 +4,7 @@ State names in a dictionary
 File needs reformatting
 """
 
+
 CODE_TO_NAME = {
     "QLD": "Queensland",
     "NSW": "New South Wales",
@@ -16,10 +17,12 @@ CODE_TO_NAME = {
 }
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+   try:
+       print(state_code, "is", CODE_TO_NAME[state_code])
+   except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+   state_code = input("Enter short state: ").upper()
+
+
