db73fd06c2f45fa814c825ebf6449ba2e713c3b6
jinyu <jinyu.xie@my.jcu.edu.au>
2025-06-19 20:00:32 +0800

    adding the main function and display function to show the list of the championship wins and winning countries

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,18 +4,23 @@ Estimate: 35 minutes
 Actual:  50 minutes
 """
 
+def main():
+    filename = "wimbledon.csv"
+    data = read_wimbledon_file(filename)
+    champion_count, countries = process_data(data)
+    display_result(champion_count, countries)
 
 
-
-def read_wimbledon_file(file):
+def read_wimbledon_file(filename):
     """read the file and extract the champion and country """
-    data=[]
-    with open(filename,"r",encoding="utf-8-sig") as file:
+    data = []
+    with open(filename, "r", encoding="utf-8-sig") as file:
+        next(file)
         for line in file:
-            parts=line.strip().split(",")
-            champion=parts[2]
-            country=parts[1]
-            data.append([champion,country])
+            parts = line.strip().split(",")
+            champion = parts[2]
+            country = parts[1]
+            data.append([champion, country])
     return data
 
 
@@ -32,3 +37,16 @@ def process_data(data):
           champion_count[champion] = 1
       countries.add(country)
   return champion_count, sorted(countries)
+
+
+def display_result(champion_count, countries):
+    """Display champions and countries"""
+    print("Wimbledon champions:")
+    for name, count in champion_count.items():
+        print(name, count)
+    print(f"\nThese {len(countries)} countries have won Wimbledon:")
+    print(", ".join(countries))
+
+
+
+main()
