6fb840205bc111b9015526972e5c775b0de569d8
jinyu <jinyu.xie@my.jcu.edu.au>
2025-06-30 20:49:47 +0800

    adding the name parameter to the Initialise and added __str__ method to return a formatted string

---
+++ prac_06/programming_language.py
@@ -0,0 +1,17 @@
+"""
+CP1404 Practical 06 - Programming Language
+Estimate: 25 minutes
+Actual:  minutes
+"""
+
+class ProgrammingLanguage:
+    """Represent information about a programming language"""
+    def __init__(self,name,typing,reflection,year):
+        """Initialize a programming language instance"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
+
