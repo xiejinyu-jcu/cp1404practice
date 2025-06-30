94a667a1799c72748d14e85f48086d951a680508
jinyu <jinyu.xie@my.jcu.edu.au>
2025-06-30 18:00:48 +0800

    create the second car called limo add the fuel and drive 115 km and show state

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -14,6 +14,12 @@ def main():
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
+    limo= Car("limo",100)
+    limo.add_fuel(20)
+    print(f"limo has fuel {limo.fuel} ")
+    limo.drive(115)
+    print(limo)
+
 
 
 main()
