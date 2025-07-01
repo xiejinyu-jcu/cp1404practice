54a07085a547bc798aca701bc0ea73a7f2d3da16
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-01 17:13:43 +0800

    create the class of Guitar and initialize fields ,accept  parameters with default values

---
+++ prac_06/guitar.py
@@ -0,0 +1,14 @@
+"""
+CP1404 Practical 06 - Guitar
+Estimate: 20 minutes
+Actual:  minutes
+"""
+
+class Guitar:
+    """create objects and define guitar properties """
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name=name
+        self.year = year
+        self.cost=cost
+
