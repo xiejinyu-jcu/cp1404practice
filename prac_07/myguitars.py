fc066b73272da6dd1d67980f719613bcdd4bbc89
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-09 17:32:28 +0800

    from the user to get the guitars information and  store in the list

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -28,7 +28,25 @@ def display_guitar(guitars):
         name_width = max(len(guitar.name) for guitar in guitars)
         year_width = max(len(str(guitar.year)) for guitar in guitars)
         cost_width = max(len(f"${guitar.cost:,.2f}") for guitar in guitars)
+        for guitar in guitars:
+            print(f"{guitar.name:<{name_width}} ({guitar.year:<{year_width}}), ${guitar.cost:>{cost_width}}")
     else:
         print("No guitars can be displayed.")
+
+def add_new_guitar(guitars):
+    """adding the new guitars name into list"""
+    name = input("Name: ").strip()
+    while name != "":
+     if any(guitar.name == name for guitar in guitars):
+        print("Guitar already exists.")
+     else:
+        try:
+            year = int(input("Year: "))
+            cost = float(input("Cost: "))
+            new_guitar = Guitar(name, year, cost)
+            guitars.append(new_guitar)
+            print(f"{new_guitar} added.")
+        except ValueError:
+            print("Invalid input,please enter the valid year and cost .")
 
 
