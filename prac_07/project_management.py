3323f669a83582534945d1515226897d1276b254
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-10 12:34:32 +0800

    read the information from the txt file and return the project objects

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -22,5 +22,18 @@ def display_menu():
           "- (Q)uit")
 
 
-
+def load_projects(filename):
+    """load the projects from the txt file"""
+    projects=[]
+    try:
+        with open(filename, 'r') as file:
+            next(file)
+            for line in file:
+                    name, start_date, priority, cost, completion_percentage = line.strip().split('\t')
+                    project = Project(name, datetime.strptime(start_date, "%d/%m/%Y").date(),
+                                      int(priority), float(cost), int(completion_percentage))
+                    projects.append(project)
+    except FileNotFoundError:
+         print(f"File {filename} not found.")
+    return projects
 
