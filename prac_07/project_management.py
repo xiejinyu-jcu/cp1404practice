319ab41038b1cac886ab8eaa5ef1d31310a0c5c6
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-10 12:17:22 +0800

    create the menu function to show menu list

---
+++ prac_07/project_management.py
@@ -0,0 +1,26 @@
+"""
+CP1404 Practical 07 - Project Management
+Estimate: 70 minutes
+Actual:  minutes
+"""
+
+from datetime import datetime
+from project import Project
+
+FILENAME = "projects.txt"
+
+
+
+def display_menu():
+    """display the menu options"""
+    print("- (L)oad projects\n"
+          "- (S)ave projects\n"
+          "- (D)isplay projects\n"
+          "- (F)ilter projects by date\n"
+          "- (A)dd new project\n"
+          "- (U)pdate project\n"
+          "- (Q)uit")
+
+
+
+
