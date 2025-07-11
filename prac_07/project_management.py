2739f71bdb460c7e85f5b3f6fb76de5eafce5c8c
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-11 16:04:08 +0800

    create the display function to show projects in the list and show their details

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,13 +1,14 @@
 """
 CP1404 Practical 07 - Project Management
 Estimate: 70 minutes
-Actual:  minutes
+Actual: 90 minutes
 """
 
 from datetime import datetime
 from project import Project
 
 FILENAME = "projects.txt"
+
 
 
 
@@ -24,17 +25,17 @@ def display_menu():
 
 def load_projects(filename):
     """load the projects from the txt file"""
-    projects=[]
+    projects = []
     try:
         with open(filename, 'r') as file:
             next(file)
             for line in file:
-                    name, start_date, priority, cost, completion_percentage = line.strip().split('\t')
-                    project = Project(name, datetime.strptime(start_date, "%d/%m/%Y").date(),
-                                      int(priority), float(cost), int(completion_percentage))
-                    projects.append(project)
+                name, start_date, priority, cost, completion_percentage = line.strip().split('\t')
+                project = Project(name, datetime.strptime(start_date, "%d/%m/%Y").date(),
+                                  int(priority), float(cost), int(completion_percentage))
+                projects.append(project)
     except FileNotFoundError:
-         print(f"File {filename} not found.")
+        print(f"File {filename} not found.")
     return projects
 
 def save_projects(projects,filename):
@@ -44,3 +45,27 @@ def save_projects(projects,filename):
         for project in projects:
             file.write(f"{project.name}\t{project.start_date.strftime('%d/%m/%Y')}\t{project.priority}\t{project.cost_estimate}\t{project.completion_percentage}\n")
 
+def display_projects(projects, filtered=False):
+    """Display all the projects and their details """
+    if not projects:
+        print("No projects to display.")
+        return
+    if not filtered:
+        incomplete_projects = sorted([project for project in projects if not project.is_complete()])
+        completed_projects = sorted([project for project in projects if project.is_complete()])
+        print("Incomplete projects:")
+        for project in incomplete_projects:
+            print(f"  {project}")
+        print("Completed projects:")
+        for project in completed_projects:
+            print(f"  {project}")
+    else:
+        for project in projects:
+            print(f"  {project}")
+
+
+
+
+
+
+main()
