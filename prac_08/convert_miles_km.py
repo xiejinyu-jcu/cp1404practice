d5492a80b22c8dc46db61f8fe10ec9606ae16ff3
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-18 16:00:45 +0800

    fix the mistake

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -11,6 +11,7 @@ from kivy.properties import StringProperty
 MILES_TO_KM = 1.60934
 
 
+
 class MilesConverterApp(App):
     """ MilesConverterApp is a Kivy App for converting miles to kilometres """
     output_text = StringProperty('')
@@ -19,7 +20,6 @@ class MilesConverterApp(App):
         """Build the Kivy app from the .kv file."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
-        # Set initial value on startup
         self.handle_calculate()
         return self.root
 
@@ -31,6 +31,7 @@ class MilesConverterApp(App):
         value = self.get_validated_miles()
         result = value * MILES_TO_KM
         self.output_text = f"{result:.4f}"
+
 
     def handle_increment(self, change):
         """
