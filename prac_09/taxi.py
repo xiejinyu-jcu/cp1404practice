9099c0c807261e34924410c019cc9d1256c9dce6
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-22 18:44:44 +0800

    call the parent clss - car  constructor function ,initialize attributes and convert to the string

---
+++ prac_09/taxi.py
@@ -0,0 +1,14 @@
+from car import Car
+
+class Taxi(Car):
+    """Specialised version of a Car that includes fare costs."""
+    price_per_km = 1.23
+
+    def __init__(self, name, fuel):
+     """Initialise a Taxi instance, based on parent class Car."""
+     super().__init__(name,fuel)
+     self.current_fare_distance = 0
+
+    def __str__(self):
+        """Return a string like a Car but with current fare distance."""
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
