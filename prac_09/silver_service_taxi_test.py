13a4ffd099fce93d8087d90b2d727ba4d7af5d78
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-24 16:01:25 +0800

    call the method of the class ,successful run the class

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -3,4 +3,11 @@ from silver_service_taxi import SilverServiceTaxi
 def test_silver_taxi():
     """ test the silver taxi class"""
     taxi=SilverServiceTaxi("Lexus", 100, fanciness=1)
+    taxi.start_fare()
+    taxi.drive(10)
+    fare = taxi.get_fare()
+    print(f"Fare for {taxi.name} is the  ${fare:.2f}")
+    print(taxi)
 
+
+test_silver_taxi()
