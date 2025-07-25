de6b95dee7b1c29134dd14f145729692cf625af1
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-25 17:56:14 +0800

    finish the main function and  adding the menu

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -7,15 +7,28 @@ MENU = 'q)uit, c)hoose taxi, d)rive'
 
 
 def main():
-    """create the main to simulator the taxi """
+    """Main function to simulate the taxi driving program."""
     taxis = [Taxi("Prius", 100),
              SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
-    current_car=None
+    current_taxi=None
     bill=0.0
     print("Let's drive!")
     print(MENU)
     choice=input("<<< ").lower()
+    while choice != 'q':
+        if choice == 'c':
+            current_taxi = choose_taxi(taxis,current_taxi, bill)
+        elif choice == 'd':
+            bill = drive_taxi(current_taxi, bill)
+        else:
+            print("Invalid option")
+            print(f"Bill to date: ${bill:.2f}")
+        print(MENU)
+        choice = input(">>> ").lower()
+    print(f"Total trip cost: ${bill:.2f}")
+    print("Taxis are now:")
+    display_taxis(taxis)
 
 
 def display_taxis(taxis):
@@ -25,6 +38,7 @@ def display_taxis(taxis):
 
 def choose_taxi(taxis,current_taxi,bill):
     """ accord the user choose,print a list of available taxis"""
+    print("Taxis available:")
     display_taxis(taxis)
     choice=int(input("Choose taxi:"))
     if choice in range(len(taxis)):
@@ -44,8 +58,10 @@ def drive_taxi(current_taxi,bill):
         distance=float(input("Drive how far? "))
         current_taxi.drive(distance)
         fare_cost=current_taxi.get_fare()
-        print(f"your {current_taxi.name} fare cost is ${fare_cost:.2f}")
+        print(f"Your {current_taxi.name} trip cost you ${fare_cost:.2f}")
         bill+=fare_cost
 
     print(f"bill to date: ${bill:.2f}")
     return  bill
+
+main()
