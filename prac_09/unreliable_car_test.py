c0d9cb4b40e80c1118cdc0b42b760a7a08fb28f5
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-25 14:52:17 +0800

    create the instance for car

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,6 @@
+from unreliable_car import  UnreliableCar
+
+def unreliable_car_test():
+    """test the unreliable car class"""
+    car=UnreliableCar("example car",100, reliability=30)
+    successful_drives = 0
