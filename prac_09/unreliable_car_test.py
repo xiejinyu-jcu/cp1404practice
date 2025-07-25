6de6ce77180c40bb6438c8bdf658c42dc9dfd092
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-25 15:04:00 +0800

    test the class , try driving car 100 times and each  time drive  1km

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -4,3 +4,11 @@ def unreliable_car_test():
     """test the unreliable car class"""
     car=UnreliableCar("example car",100, reliability=30)
     successful_drives = 0
+    attempts_drives = 100
+    for i in range(attempts_drives):
+          distance=car.drive(1)
+          if distance > 0:
+             successful_drives += 1
+    print(f"Successful drives: {successful_drives} out of {attempts_drives}")
+
+unreliable_car_test()
