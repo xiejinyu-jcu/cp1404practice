47726e7a5931ca2adf2721e2149ca5fbc0a9264c
jinyu <jinyu.xie@my.jcu.edu.au>
2025-07-25 17:45:09 +0800

    check if the user choose the taxi ,if choose adding to the bill

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -12,7 +12,7 @@ def main():
              SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
     current_car=None
-    total_bill=0.0
+    bill=0.0
     print("Let's drive!")
     print(MENU)
     choice=input("<<< ").lower()
@@ -23,7 +23,7 @@ def display_taxis(taxis):
     for i,taxi in enumerate(taxis):
         print(f"{i}-{taxi}")
 
-def choose_taxi(taxis):
+def choose_taxi(taxis,current_taxi,bill):
     """ accord the user choose,print a list of available taxis"""
     display_taxis(taxis)
     choice=int(input("Choose taxi:"))
@@ -31,9 +31,21 @@ def choose_taxi(taxis):
         current_taxi=taxis[choice]
     else:
         print("invalid taxi choice")
-    print(f"bill to date: ${total_bill:.2f}")
+    print(f"bill to date: ${bill:.2f}")
     return current_taxi
 
 
+def drive_taxi(current_taxi,bill):
+    """simulate the drive and calculate the fare"""
+    if current_taxi is None:
+        print("You need to choose a taxi before you can drive")
+    else:
+        current_taxi.start_fare()
+        distance=float(input("Drive how far? "))
+        current_taxi.drive(distance)
+        fare_cost=current_taxi.get_fare()
+        print(f"your {current_taxi.name} fare cost is ${fare_cost:.2f}")
+        bill+=fare_cost
 
-
+    print(f"bill to date: ${bill:.2f}")
+    return  bill
